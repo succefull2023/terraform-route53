9664a0c8c15dd09eff3c6a2d8fe2a1a11d7c6387
Eric <evfomo@gmail.com>
2023-04-02 17:37:41 -0500

    terraform code

---
+++ data.tf
@@ -0,0 +1,5 @@
+data "aws_security_group" "sg1" {
+   name = "webserver"
+   // id = "sg-0281902daebeb54c5"
+
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ami-type" {
+  description = "ami info to get get from aws"
+  type = string
+  default = "ami-04581fbf744a7d11f"
+}
+variable "instance_type" {
+    default = "t2.micro"
+}
+variable "env" {
+  default = "dev"
+}
+variable "instance-name" {
+  default = "Postgresql-server-dev"
+}
